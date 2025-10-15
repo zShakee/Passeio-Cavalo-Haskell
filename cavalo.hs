37b012d96b3ba3edb305c149ebd8a7f9354259b5
Luiggi Gerevini <63132089+zShakee@users.noreply.github.com>
2025-10-15 20:03:34 -0300

    Add files via upload

--- cavalo.hs
+++ cavalo.hs
@@ -1,6 +1,13 @@
+
+
+movimentosPossiveis x y linhas colunas = 
+    let moves = [ (-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+        todosMovimentos = map (\(dx, dy) -> (x + dx, y + dy)) moves
+    in filter (\(nx, ny) -> nx >= 0 && nx <= linhas && ny >= 0 && ny <= colunas) todosMovimentos 
+
 main :: IO ()
 main = do
-    conteudo <- readFile "teste.txt"
+    conteudo <- readFile "entrada.txt"
     let valores = map read (words conteudo) :: [Integer]
     let [n, m, x, y] = valores
     putStrLn $ "Dimensões do tabuleiro: " ++ show n ++ "x" ++ show m
